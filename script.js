e430c93873406e7683bc6745e7bdca8f8e1cfa94
Shalu <88194182+shalujha123@users.noreply.github.com>
2025-04-12 22:00:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,18 @@
 //your code here
 
+let input = document.getElementById("newTodoInput")
+let btn = document.getElementById("addTodoBtn")
+let output = document.getElementById("todoList")
+
+btn.addEventListener("click",addTask)
+
+function addTask(){
+	let task = input.value.trim()
+	if(!task){
+		alert("Add item to list")
+	}
+	let li = document.createElement("li")
+	li.innerHTML = task
+	output.append(li)
+	input.value = ""
+}
